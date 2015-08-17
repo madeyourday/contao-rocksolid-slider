f075f8dd22f240886e42632ff19d243bf1532c94
Martin Auswöger <martin@auswoeger.com>
2015-08-17 11:55:52 +0200

    Escaped database default values for Contao 4

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -393,7 +393,9 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['rsts_thumbs_imgSize'] = array(
 		'helpwizard' => true,
 		'tl_class' => 'w50',
 	),
-	'sql' => "varchar(64) NOT NULL default 'a:3:{i:0;s:2:\"50\";i:1;s:2:\"50\";i:2;s:4:\"crop\";}'",
+	'sql' => version_compare(VERSION, '4', '<')
+		? "varchar(64) NOT NULL default 'a:3:{i:0;s:2:\"50\";i:1;s:2:\"50\";i:2;s:4:\"crop\";}'"
+		: "varchar(64) NOT NULL default 'a:3:{i:0;s:2:\\\"50\\\";i:1;s:2:\\\"50\\\";i:2;s:4:\\\"crop\\\";}'",
 );
 $GLOBALS['TL_DCA']['tl_content']['fields']['rsts_thumbs_slideMaxCount'] = array(
 	'label' => &$GLOBALS['TL_LANG']['tl_module']['rsts_slideMaxCount'],
