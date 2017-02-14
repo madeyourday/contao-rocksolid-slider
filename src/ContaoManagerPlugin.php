58d98475ad0ee39efbffb6b13c6d8f05813d07b2
Martin Auswöger <martin@auswoeger.com>
2017-02-14 17:23:58 +0100

    Add Contao Manager plugin

---
+++ src/ContaoManagerPlugin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MadeYourDay\RockSolidSlider;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+
+class ContaoManagerPlugin implements BundlePluginInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getBundles(ParserInterface $parser)
+	{
+		return [new BundleConfig(RockSolidSliderBundle::class)];
+	}
+}
