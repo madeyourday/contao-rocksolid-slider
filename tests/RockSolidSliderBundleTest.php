fecb97ac6a2f817fca393c21a053423f03936afe
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-05-22 20:24:09 +0200

    Add bundle test

---
+++ tests/RockSolidSliderBundleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Copyright MADE/YOUR/DAY OG <mail@madeyourday.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MadeYourDay\RockSolidSlider\Test;
+
+use MadeYourDay\RockSolidSlider\RockSolidSliderBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Tests the RockSolidSliderBundle class.
+ *
+ * @author Christian Schiffler <c.schiffler@cyberspectrum.de>
+ */
+class RockSolidSliderBundleTest extends TestCase
+{
+    /**
+     * Tests the object instantiation.
+     */
+    public function testInstantiation()
+    {
+        $bundle = new RockSolidSliderBundle();
+
+        $this->assertInstanceOf('MadeYourDay\RockSolidSlider\RockSolidSliderBundle', $bundle);
+    }
+}
