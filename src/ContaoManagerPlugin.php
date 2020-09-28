7acd689da80400e1c95490eafd12722894f4d36e
Martin Auswöger <martin@auswoeger.com>
2020-09-28 11:40:11 +0200

    Fix bundle loading order

--- src/ContaoManagerPlugin.php
+++ src/ContaoManagerPlugin.php
@@ -2,10 +2,12 @@
 
 namespace MadeYourDay\RockSolidSlider;
 
+use Contao\CalendarBundle\ContaoCalendarBundle;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\NewsBundle\ContaoNewsBundle;
 
 class ContaoManagerPlugin implements BundlePluginInterface
 {
@@ -17,6 +19,8 @@ class ContaoManagerPlugin implements BundlePluginInterface
 		return [
 			BundleConfig::create(RockSolidSliderBundle::class)
 				->setLoadAfter([ContaoCoreBundle::class])
+				->setLoadAfter([ContaoNewsBundle::class])
+				->setLoadAfter([ContaoCalendarBundle::class])
 				->setReplace(['rocksolid-slider']),
 		];
 	}
